a62b71975a513e4be873a1f46dbf923989f18fb2
Gabriel Gambôa <gabriel_gamboa10@hotmail.com>
2022-06-27 11:13:39 -0300

    testando configurations

--- src/main/java/com/example/vendasApi/VendasApiApplication.java
+++ src/main/java/com/example/vendasApi/VendasApiApplication.java
@@ -1,10 +1,24 @@
 package com.example.vendasApi;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class VendasApiApplication {
+
+	@Autowired
+	@Qualifier("appName")
+	public String appName;
+
+	@GetMapping("/hello")
+	public String oi() {
+		return appName;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(VendasApiApplication.class, args);
---
+++ src/main/java/com/example/vendasApi/configs/MyConfiguration.java
@@ -0,0 +1,13 @@
+package com.example.vendasApi.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyConfiguration {
+
+    @Bean(name = "appName")
+    public String applicationName() {
+        return "Sistema de vendas";
+    }
+}
